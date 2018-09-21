01a45ddad083059473780192b39c852904e8f175
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2018-09-21 16:09:43 -0300

    first files

---
+++ collect_abstracts.py
@@ -0,0 +1,45 @@
+import glob
+import csv
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pandas as pd
+
+path = "dbpedia/treinamento/*.csv"
+files = glob.glob(path)
+dataset = []
+
+for file in files:
+    with open(file, encoding="utf8") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            if (line[1] != 'entity'):
+                entity = line[1].replace('DBPEDIA_ID/', '')
+                # remover virgulas do nome da entidade
+                label_target = file[20:].replace('.csv', '')
+                labels = line[2]
+
+                # consulta para recuperar o resumo
+                query = "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> " \
+                        " select * where" \
+                        " { <http://dbpedia.org/resource/" + entity + "> dbpedia-owl:abstract ?abstract " \
+                        "filter(langMatches(lang(?abstract),\"en\")) }"
+
+                # extrair o resumo para uma variável
+                sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+                sparql.setQuery(query)
+                sparql.setReturnFormat(JSON)
+                results = sparql.query().convert()
+                for result in results["results"]["bindings"]:
+                    for key, value in result.items():
+                        abstract = (value['value'].replace("\n", ''))
+                # exibir a tupla no formato "entidade, label_alvo, labels, resumo"
+                tuple = [entity, label_target, labels, abstract]
+                dataset.append(tuple)
+                print(tuple)
+        #break
+
+# gerando csv de saída...
+colunas = ['entity', 'class_target', 'other_class', 'abstract']
+df = pd.DataFrame(dataset, columns=colunas)
+df.to_csv('file_output.csv')
+
+
