cda87790ec1a2c681a3afe68eb80d95c209840eb
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2018-09-21 22:36:48 -0300

    alterando script para pegar apenas 200 exemplos de cada entidade

--- collect_abstracts.py
+++ collect_abstracts.py
@@ -6,15 +6,19 @@ import requests
 from urllib import request, error
 from requests.exceptions import HTTPError
 from tqdm import tqdm
+import time
 
 path = "dbpedia/treinamento/*.csv"
 files = glob.glob(path)
 dataset = []
-
+colunas = ['entity', 'class_target', 'other_class', 'abstract']
+data_frame = pd.DataFrame()
 for file in tqdm(files):
     with open(file, encoding="utf8") as f:
         reader = csv.reader(f)
-        for line in reader:
+        i = 0
+        time.sleep(5)
+        for line in tqdm(reader):
             if (line[1] != 'entity'):
                 entity = line[1].replace('DBPEDIA_ID/', '')
                 # remover virgulas do nome da entidade
@@ -33,13 +37,26 @@ for file in tqdm(files):
                     sparql.setQuery(query)
                     sparql.setReturnFormat(JSON)
                     results = sparql.query().convert()
+                    abstract = tuple()
                     for result in results["results"]["bindings"]:
                         for key, value in result.items():
                             abstract = (value['value'].replace("\n", ''))
                     # exibir a tupla no formato "entidade, label_alvo, labels, resumo"
-                    tuple = [entity, label_target, labels, abstract]
-                    dataset.append(tuple)
+                    tuple_ = [entity, label_target, labels, abstract]
+                    dataset.append(tuple_)
                     print(tuple)
+                    if(i % 200 == 0):
+                        colunas = ['entity', 'class_target', 'other_class', 'abstract']
+                        if len(data_frame) == 0:
+                            data_frame = pd.DataFrame(dataset, columns=colunas)
+                        else:
+                            data_frame2 = pd.DataFrame(dataset, columns=colunas)
+                            data_frame = pd.read_csv('file_output.csv')
+                            data_frame = np.concatenate([data_frame,data_frame2])
+
+                        data_frame.to_csv('file_output.csv')
+                        break
+                    i+=1
                 except error.HTTPError as err:
                     if err.code == 404:
                         print('Error entity:{0}'.format(entity))
