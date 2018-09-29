8525b64cc1c97275b2166a7bc838146fcda7fc2d
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2018-09-29 15:27:30 -0300

    correção

--- collect_abstracts.py
+++ collect_abstracts.py
@@ -36,7 +36,7 @@ for file in tqdm(files):
                     print(label_target) 
                     break
                 
-                print('Não foi baixado ainda:{0}'.format(label_target))
+                print(label_target)
                                
 #               consulta para recuperar o resumo
                 query = "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> " \
