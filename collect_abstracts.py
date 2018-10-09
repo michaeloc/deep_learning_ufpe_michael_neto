fd2323690b215ff6cc184155d0c040d3e7de3c35
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2018-10-09 20:38:06 -0300

    alterando para baixar o quinto batch

--- collect_abstracts.py
+++ collect_abstracts.py
@@ -20,6 +20,7 @@ data_frame = pd.DataFrame()
 downloaded_data = pd.read_csv('file_output_1000_example.csv')
 downloaded_data2 = pd.read_csv('file_output_1000_example2.csv')
 downloaded_data3 = pd.read_csv('file_output_1000_example3.csv')
+downloaded_data4 = pd.read_csv('file_output_1000_example4.csv')
 unique_class_downloaded = np.unique(downloaded_data.class_target.values)
 print(unique_class_downloaded)
 for file in tqdm(files):
@@ -36,7 +37,9 @@ for file in tqdm(files):
                 label_target = file[20:].replace('.csv', '')
                 labels = line[2]
 
-                if (label_target in unique_class_downloaded) or (label_target in downloaded_data2) or (label_target in downloaded_data3):
+                if (label_target in unique_class_downloaded) or 
+                    (label_target in downloaded_data2) or 
+                    (label_target in downloaded_data3) or (label_target in downloaded_data4):
                     print('ja baixou')
                     print(label_target) 
                     break
@@ -71,7 +74,7 @@ for file in tqdm(files):
                             # data_frame = pd.read_csv('file_output.csv',index_col=0)
                             data_frame2 = pd.DataFrame(dataset, columns=colunas)
                             data_frame = pd.concat([data_frame,data_frame2],ignore_index=True)
-                        data_frame.to_csv('file_output_1000_example4.csv')
+                        data_frame.to_csv('file_output_1000_example5.csv')
                         break
                     i+=1
                 except error.HTTPError as err:
@@ -79,7 +82,7 @@ for file in tqdm(files):
                         print('Error entity:{0}'.format(entity))
                         colunas = ['entity', 'class_target', 'other_class', 'abstract']
                         df = pd.DataFrame(dataset, columns=colunas)
-                        df.to_csv('file_output_1000_example4.csv')
+                        df.to_csv('file_output_1000_example5.csv')
                     else:
                         raise
                     
@@ -88,6 +91,6 @@ for file in tqdm(files):
 # gerando csv de saída...
 colunas = ['entity', 'class_target', 'other_class', 'abstract']
 df = pd.DataFrame(dataset_out, columns=colunas)
-df.to_csv('file_output_1000_example4.csv')
+df.to_csv('file_output_1000_example5.csv')
 
 
