ab196be1bcb9a40f3e6cb17d18f9de549fa8e0e0
Michael Cruz <michaelcruz@Michaels-MacBook-Pro.local>
2018-10-17 09:40:25 -0300

    alterando codigo para baixar as entidades que faltam para o experimento

--- collect_abstracts.py
+++ collect_abstracts.py
@@ -11,24 +11,25 @@ from tqdm import tqdm
 import time
 import numpy as np
 
-path = "dbpedia/treinamento/*.csv"
+# path = "dbpedia/treinamento/*.csv"
+path = "arquivos_experimentos/*.csv"
 files = glob.glob(path)
 colunas = ['entity', 'class_target', 'other_class', 'abstract']
 dataset_out = []
 data_frame = pd.DataFrame()
 
-downloaded_data1 = pd.read_csv('file_output_1000_example1.csv')
-downloaded_data2 = pd.read_csv('file_output_1000_example2.csv')
-downloaded_data3 = pd.read_csv('file_output_1000_example3.csv')
+# downloaded_data1 = pd.read_csv('file_output_1000_example1.csv')
+# downloaded_data2 = pd.read_csv('file_output_1000_example2.csv')
 # downloaded_data3 = pd.read_csv('file_output_1000_example3.csv')
-downloaded_data4 = pd.read_csv('file_output_1000_example4.csv')
+# downloaded_data3 = pd.read_csv('file_output_1000_example3.csv')
+# downloaded_data4 = pd.read_csv('file_output_1000_example4.csv')
 # downloaded_data5 = pd.read_csv('file_output_1000_example5.csv')
 # downloaded_data6 = pd.read_csv('file_output_1000_example6.csv')
 # downloaded_data7 = pd.read_csv('file_output_1000_example7.csv')
-unique_class_downloaded1 = np.unique(downloaded_data1.class_target.values)
-unique_class_downloaded4 = np.unique(downloaded_data4.class_target.values)
-unique_class_downloaded2 = np.unique(downloaded_data2.class_target.values)
-unique_class_downloaded3 = np.unique(downloaded_data3.class_target.values)
+# unique_class_downloaded1 = np.unique(downloaded_data1.class_target.values)
+# unique_class_downloaded4 = np.unique(downloaded_data4.class_target.values)
+# unique_class_downloaded2 = np.unique(downloaded_data2.class_target.values)
+# unique_class_downloaded3 = np.unique(downloaded_data3.class_target.values)
 # print(unique_class_downloaded)
 for file in tqdm(files):
     with open(file, encoding="utf8") as f:
@@ -39,19 +40,21 @@ for file in tqdm(files):
   
         for line in reader:
             if (line[1] != 'entity'):
-                entity = line[1].replace('DBPEDIA_ID/', '')
+                # entity = line[1].replace('DBPEDIA_ID/', '')
+                entity = line[1]
                 # remover virgulas do nome da entidade
-                label_target = file[20:].replace('.csv', '')
+                # label_target = file[20:].replace('.csv', '')
+                label_target = line[2]
                 labels = line[2]
 
-                if (label_target in unique_class_downloaded1) or \
-                    (label_target in unique_class_downloaded2) or \
-                    (label_target in unique_class_downloaded3) or \
-                    (label_target in unique_class_downloaded4):
-                    print('ja baixou')
-                    print(label_target) 
-                    break
-                
+                # if (label_target in unique_class_downloaded1) or \
+                #     (label_target in unique_class_downloaded2) or \
+                #     (label_target in unique_class_downloaded3) or \
+                #     (label_target in unique_class_downloaded4):
+                #     print('ja baixou')
+                #     print(label_target) 
+                #     break
+                print(label_target.encode('utf-8'))
                 print(entity.encode('utf-8'))
 #               consulta para recuperar o resumo
                 query = "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> " \
@@ -86,7 +89,7 @@ for file in tqdm(files):
                             # data_frame = pd.read_csv('file_output.csv',index_col=0)
                             data_frame2 = pd.DataFrame(dataset, columns=colunas)
                             data_frame = pd.concat([data_frame,data_frame2],ignore_index=True)
-                        data_frame.to_csv('file_output_1000_example5.csv')
+                        data_frame.to_csv('file_output_differences.csv')
                         dataset = []
                         # print('salvando:{0}'.format(label_target))
                         print('Tamanho da base:{0}'.format(len(data_frame)))
@@ -96,7 +99,7 @@ for file in tqdm(files):
                         print('Error entity:{0}'.format(entity))
                         colunas = ['entity', 'class_target', 'other_class', 'abstract']
                         df = pd.DataFrame(dataset, columns=colunas)
-                        df.to_csv('file_output_1000_example5.csv')
+                        df.to_csv('file_output_differences.csv')
                     else:
                         raise
                     
@@ -105,6 +108,6 @@ for file in tqdm(files):
 # gerando csv de saída...
 colunas = ['entity', 'class_target', 'other_class', 'abstract']
 df = pd.DataFrame(dataset_out, columns=colunas)
-df.to_csv('file_output_1000_example5.csv')
+df.to_csv('file_output_differences.csv')
 
 
